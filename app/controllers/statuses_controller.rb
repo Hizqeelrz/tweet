f917cd41a815bd47a20d11b4a372f3afaf9967f0
hizqeel <hizqeel1214@gmail.com>
2015-05-03 21:25:06 +0500

    add design to status

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,4 +1,25 @@
 class StatusesController < ApplicationController
+	before_action :authenticate_user!
+
 	def index
+		@status = Status.new
+		@statuses = current_user.statuses.all
+	end
+
+	def create
+		@status = Status.new(status_params)
+		@status.user = current_user
+		if @status.save
+			flash[:success] = "status is saved!"
+			redirect_to statuses_path
+		else
+			flash[:error] = "status not saved!"
+			redirect_to statuses_path
+		end
+	end
+
+	private
+	def status_params
+		params.require(:status).permit(:body)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+  resources :profiles
   resources :statuses
 
   devise_for :users
